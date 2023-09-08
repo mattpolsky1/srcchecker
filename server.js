e4c1126173862be5d832825d47d13ed321c7712c
UNC Support <uncsupport@UNCs-MacBook-Air.local>
2023-09-08 11:34:48 -0400

    does this work

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const path = require('path'); // Import the 'path' module
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,14 +11,13 @@ let totalCheckIns = 0;
 let lastCheckInTime = 0;
 
 // Define the path to your static files directory
-const publicPath = path.join(__dirname, 'Public'); // Use 'Public' with a capital 'P'
+const publicPath = path.join(__dirname, 'Public');
 
 // Serve static files from the "Public" directory
 app.use(express.static(publicPath));
 
 // Route handler for the root URL ("/")
 app.get('/', (req, res) => {
-    // Use path.join to ensure the correct path to 'index.html'
     const indexPath = path.join(publicPath, 'index.html');
     res.sendFile(indexPath);
 });
@@ -36,8 +35,8 @@ io.on('connection', (socket) => {
 
     socket.on('checkIn', () => {
         updatePeopleCount();
-        totalCheckIns++; // Increase the total check-ins count
-        io.emit('updateCount', totalCheckIns); // Broadcast the updated count to all clients
+        totalCheckIns++;
+        io.emit('updateCount', totalCheckIns);
     });
 
     socket.on('disconnect', () => {
@@ -45,11 +44,11 @@ io.on('connection', (socket) => {
     });
 });
 
-// Use the process.env.PORT variable provided by Heroku
+
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-updatePeopleCount(); // Initialize the lastCheckInTime and totalCheckIns
+updatePeopleCount();
