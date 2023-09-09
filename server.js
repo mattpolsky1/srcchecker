462612960714c3927ff7bdf50031307069ecd182
UNC Support <uncsupport@UNCs-MacBook-Air.local>
2023-09-08 21:18:09 -0400

    normal

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 let totalCheckIns = 0;
+const checkInTimeout = 10 * 1000; // 10 seconds timeout for check-ins
 
 // Define the path to your static files directory
 const publicPath = path.join(__dirname, 'Public');
@@ -21,17 +22,38 @@ app.get('/', (req, res) => {
     res.sendFile(indexPath);
 });
 
+function updatePeopleCount() {
+    // Check if users are still checked in and automatically check them out
+    const currentTime = Date.now();
+    io.sockets.clients().forEach((socket) => {
+        const checkInTime = socket.checkInTime || 0;
+        if (currentTime - checkInTime >= checkInTimeout) {
+            socket.checkInTime = 0; // Reset check-in time
+            if (totalCheckIns > 0) {
+                totalCheckIns--;
+            }
+            io.emit('updateCount', totalCheckIns);
+        }
+    });
+}
+
 io.on('connection', (socket) => {
     socket.emit('updateCount', totalCheckIns);
 
     socket.on('checkIn', () => {
         totalCheckIns++;
+        socket.checkInTime = Date.now(); // Store check-in time
         io.emit('updateCount', totalCheckIns);
     });
 
     socket.on('disconnect', () => {
-        totalCheckIns--;
-        io.emit('updateCount', totalCheckIns);
+        if (socket.checkInTime) {
+            socket.checkInTime = 0; // Reset check-in time
+            if (totalCheckIns > 0) {
+                totalCheckIns--;
+            }
+            io.emit('updateCount', totalCheckIns);
+        }
     });
 });
 
@@ -40,3 +62,5 @@ const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+setInterval(updatePeopleCount, 1000); // Check for expired check-ins every second
