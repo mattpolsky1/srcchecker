8c02de7e078eda085d51587289ac1689c2c8b448
UNC Support <uncsupport@UNCs-MacBook-Air.local>
2023-09-13 00:25:50 -0400

    final changes

--- server.js
+++ server.js
@@ -36,16 +36,22 @@ io.on('connection', (socket) => {
     socket.on('checkIn', (userLocation) => {
         updatePeopleCount();
 
-        // Calculate the distance between user's location and the target location
-        const targetLocation = { latitude: 35.90927, longitude: -79.04746 };
-        const distance = getDistance(userLocation, targetLocation);
+        // Check if geolocation data is available
+        if (userLocation && userLocation.latitude && userLocation.longitude) {
+            // Calculate the distance between user's location and the target location
+            const targetLocation = { latitude: 35.90927, longitude: -79.04746 };
+            const distance = getDistance(userLocation, targetLocation);
 
-        // Check if the user is within 2 miles of the target location (3218.69 meters)
-        if (distance <= 3218.69) {
-            totalCheckIns++;
-            io.emit('updateCount', totalCheckIns);
+            // Check if the user is within 2 miles of the target location (3218.69 meters)
+            if (distance <= 3218.69) {
+                totalCheckIns++;
+                io.emit('updateCount', totalCheckIns);
+            } else {
+                // Notify the client that check-in is not allowed
+                socket.emit('checkInNotAllowed');
+            }
         } else {
-            // Notify the client that check-in is not allowed
+            // Handle the case where geolocation data is not available
             socket.emit('checkInNotAllowed');
         }
     });
@@ -88,3 +94,4 @@ const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
