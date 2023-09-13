c0250d81adc7d1d56a72144c834ec446e969947f
UNC Support <uncsupport@UNCs-MacBook-Air.local>
2023-09-12 22:58:07 -0400

    try

--- server.js
+++ server.js
@@ -23,23 +23,23 @@ app.get('/', (req, res) => {
 });
 
 // Include your Google Maps API key here
-const apiKey = 'AIzaSyBrWbBamD1cA1NL9Q3Mt1Izml4WwZlzncs';
+const apiKey = 'AIzaSyBrWbBamD1cA1NL9Q3Mt1Izml4WwZlzncs'; // Replace with your actual API key
 
 // Middleware to calculate distance between two coordinates
 function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Radius of the Earth in kilometers
-  const dLat = (lat2 - lat1) * (Math.PI / 180);
-  const dLon = (lon2 - lon1) * (Math.PI / 180);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(lat1 * (Math.PI / 180)) *
-    Math.cos(lat2 * (Math.PI / 180)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c; // Distance in kilometers
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distance in kilometers
 
-  // Convert distance to miles
-  return distance * 0.621371;
+    // Convert distance to miles
+    return distance * 0.621371;
 }
 
 function updatePeopleCount() {
@@ -69,15 +69,15 @@ io.on('connection', (socket) => {
             targetLongitude
         );
 
-        // Check if the user is within 1 mile of the target location
-        if (distance <= 1) {
+        // Check if the user is within 0.1 miles of the target location
+        if (distance <= 0.1) {
             // Increment the count only if the check-in is successful
             updatePeopleCount();
             totalCheckIns++;
             io.emit('updateCount', totalCheckIns);
         } else {
-            // Inform the user that they are not within 1 mile of the target location.
-            socket.emit('checkInFailed', 'You are not within 1 mile of SRC (Student Recreation Center). Check-in not allowed.');
+            // Inform the user that they are not within 0.1 miles of the target location.
+            socket.emit('checkInFailed', 'You are not within 0.1 miles of the specified location. Check-in not allowed.');
         }
     });
 
