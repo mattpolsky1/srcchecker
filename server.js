62e99f7e6c73f4e1487cf9121be60e8db93c985b
dolphins <dolphins@dolphinss-air.wireless-1x.unc.edu>
2023-11-29 10:37:33 -0500

    back to normal without mongo, if doesnt work go back

--- server.js
+++ server.js
@@ -2,28 +2,9 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://mattpolsky:Manning01!@cluster0.ev0u1hj.mongodb.net/CampusHoops?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensure that the client will close when you finish/error
-        // await client.close();
-    }
-}
+// Require the express-force-https middleware
+const forceHttps = require('express-force-https');
 
 const app = express();
 const server = http.createServer(app);
@@ -32,12 +13,22 @@ const io = socketIO(server);
 let totalCheckIns = 60;
 let lastCheckInTime = 0;
 
-const connectedUsers = new Map();
+// Create a map to store checked-in users
+const checkedInUsers = new Map();
+
+// Create a map to store the last check-in time for each user
 const lastCheckInTimes = new Map();
 
+// Define the path to your static files directory
 const publicPath = path.join(__dirname, 'Public');
+
+// Serve static files from the "Public" directory
 app.use(express.static(publicPath));
 
+// Add the forceHttps middleware before other route handlers
+app.use(forceHttps);
+
+// Route handler for the root URL ("/")
 app.get('/', (req, res) => {
     const indexPath = path.join(publicPath, 'index.html');
     res.sendFile(indexPath);
@@ -45,194 +36,100 @@ app.get('/', (req, res) => {
 
 function updatePeopleCount() {
     const currentTime = Date.now();
+
+    // Get the current time in Eastern Time (UTC-5)
     const currentTimeInET = new Date(currentTime - 5 * 60 * 60 * 1000);
+
+    // Define the reset time (8:00 PM in ET)
     const resetTimeInET = new Date(currentTimeInET);
     resetTimeInET.setHours(20, 0, 0, 0);
 
     if (currentTimeInET >= resetTimeInET) {
+        // Reset the count at 8:00 PM ET
         totalCheckIns = 0;
     }
 
     lastCheckInTime = currentTime;
 }
 
-io.on('connection', async (socket) => {
-    try {
-        socket.emit('initCount', totalCheckIns);
+io.on('connection', (socket) => {
+    // Emit the current totalCheckIns count to the newly connected user
+    socket.emit('updateCount', totalCheckIns);
 
-        if (connectedUsers.has(socket.id)) {
+    // Check if the user is already checked in based on their socket ID
+    if (checkedInUsers.has(socket.id)) {
+        socket.emit('alreadyCheckedIn');
+    }
+
+    socket.on('checkIn', (userLocation) => {
+        updatePeopleCount();
+
+        // Check if the user is already checked in
+        if (checkedInUsers.has(socket.id)) {
             socket.emit('alreadyCheckedIn');
-        }
-
-        socket.on('disconnect', () => {
-            const socketId = socket.id;
-
-            if (connectedUsers.has(socketId)) {
+        } else {
+            // Check if the user has a last check-in time recorded
+            if (lastCheckInTimes.has(socket.id)) {
                 const currentTime = Date.now();
-                const lastCheckInTime = lastCheckInTimes.get(socketId);
+                const lastCheckInTime = lastCheckInTimes.get(socket.id);
                 const timeSinceLastCheckIn = currentTime - lastCheckInTime;
 
+                // Check if the user is attempting to check in before the cooldown period (30 seconds) has passed
                 if (timeSinceLastCheckIn < 30000) {
-                    // Calculate the remaining time on the timer
-                    const remainingTime = 30000 - timeSinceLastCheckIn;
+                    socket.emit('checkInCooldown', 30000 - timeSinceLastCheckIn);
+                    return; // Exit the function, preventing the check-in
+                }
+            }
 
-                    // Emit an event to notify the client about the automatic check-out
-                    socket.emit('checkedOutAutomatically', remainingTime);
+            // Check if geolocation data is available
+            if (userLocation && userLocation.latitude && userLocation.longitude) {
+                // Calculate the distance between user's location and the target location
+                const targetLocation = { latitude: 35.90927, longitude: -79.04746 };
+                const distance = getDistance(userLocation, targetLocation);
 
-                    // Set a timer to decrease the count after the remaining time
+                // Check if the user is within 10 miles of the target location (3218.69 meters)
+                if (distance <= 10000) {
+                    // Mark the user as checked in, store their socket ID, and record the check-in time
+                    checkedInUsers.set(socket.id, true);
+                    totalCheckIns++;
+                    io.emit('updateCount', totalCheckIns);
+
+                    lastCheckInTimes.set(socket.id, Date.now()); // Record the check-in time
+
+                    // Automatically check out the user after 30 seconds
                     setTimeout(() => {
-                        // If the user has checked in before, remove them and decrement the count
-                        if (connectedUsers.has(socketId)) {
-                            connectedUsers.delete(socketId);
+                        if (checkedInUsers.get(socket.id)) {
+                            checkedInUsers.delete(socket.id);
                             totalCheckIns--;
                             io.emit('updateCount', totalCheckIns);
+                            socket.emit('checkedOutAutomatically');
+                            socket.emit('checkOut'); // Emit 'checkOut' event to update client
                         }
-
-                        // Disconnect the socket after the remaining time
-                        socket.disconnect(true);
-                    }, remainingTime);
-                }
-            }
-        });
-
-        socket.on('checkIn', async (userLocation) => {
-            try {
-                updatePeopleCount();
-
-                if (connectedUsers.has(socket.id)) {
-                    socket.emit('alreadyCheckedIn');
+                    }, 30000); // 30 seconds
                 } else {
-                    if (lastCheckInTimes.has(socket.id)) {
-                        const currentTime = Date.now();
-                        const lastCheckInTime = lastCheckInTimes.get(socket.id);
-                        const timeSinceLastCheckIn = currentTime - lastCheckInTime;
-
-                        if (timeSinceLastCheckIn < 30000) {
-                            socket.emit('checkInCooldown', 30000 - timeSinceLastCheckIn);
-                            return;
-                        }
-                    }
-
-                    if (userLocation && userLocation.latitude && userLocation.longitude) {
-                        const targetLocation = { latitude: 35.90927, longitude: -79.04746 };
-                        const distance = getDistance(userLocation, targetLocation);
-
-                        if (distance <= 10000) {
-                            connectedUsers.set(socket.id, true);
-                            totalCheckIns++;
-                            io.emit('updateCount', totalCheckIns);
-
-                            lastCheckInTimes.set(socket.id, Date.now());
-
-                            const db = client.db('CampusHoops');
-                            const collection = db.collection('Data');
-
-                            const checkInData = {
-                                socketId: socket.id,
-                                checkInTime: new Date(),
-                                userLocation: userLocation
-                            };
-
-                            try {
-                                await collection.insertOne(checkInData);
-                                console.log('Check-in data inserted into MongoDB');
-                            } catch (error) {
-                                console.error('Error inserting check-in data into MongoDB:', error);
-                            }
-                        } else {
-                            socket.emit('checkInNotAllowed');
-                        }
-                    } else {
-                        socket.emit('checkInNotAllowed');
-                    }
+                    // Notify the client that check-in is not allowed
+                    socket.emit('checkInNotAllowed');
                 }
-            } catch (error) {
-                console.error('Error handling check-in:', error);
-            }
-        });
-
-        socket.on('checkOut', () => {
-            const socketId = socket.id;
-
-            // If the user has checked in before, remove them and decrement the count
-            if (connectedUsers.has(socketId)) {
-                connectedUsers.delete(socketId);
-                totalCheckIns--;
-                io.emit('updateCount', totalCheckIns);
             } else {
-                // If the user has not checked in before (maybe due to page refresh), decrement the count anyway
-                totalCheckIns--;
-                io.emit('updateCount', totalCheckIns);
-            }
-        });
-
-        socket.on('requestInitialCount', () => {
-            socket.emit('initCount', totalCheckIns);
-        });
-
-        async function logDailyCheckIns() {
-            try {
-                const db = client.db('CampusHoops');
-                const collection = db.collection('Data');
-
-                const currentDate = getCurrentDate();
-
-                const dailyCheckIns = await collection.find({
-                    checkInTime: {
-                        $gte: currentDate,
-                        $lt: new Date(currentDate.getTime() + 24 * 60 * 60 * 1000)
-                    }
-                }).toArray();
-
-                console.log('Number of check-ins today:', dailyCheckIns.length);
-                io.emit('dailyCheckIns', { date: currentDate, count: dailyCheckIns.length });
-            } catch (error) {
-                console.error('Error logging daily check-ins:', error);
+                // Handle the case where geolocation data is not available
+                socket.emit('checkInNotAllowed');
             }
         }
+    });
 
-        async function logHourlyCheckIns() {
-            try {
-                const db = client.db('CampusHoops');
-                const collection = db.collection('Data');
-
-                const currentHour = getCurrentHour();
-
-                if (currentHour >= 8 && currentHour <= 20) {
-                    const hourlyCheckIns = await collection.find({
-                        checkInTime: {
-                            $gte: new Date().setHours(8, 0, 0),
-                            $lt: new Date().setHours(21, 0, 0)
-                        }
-                    }).toArray();
-
-                    console.log('Number of check-ins this hour:', hourlyCheckIns.length);
-                    io.emit('hourlyCheckIns', { hour: currentHour, count: hourlyCheckIns.length });
-                }
-            } catch (error) {
-                console.error('Error logging hourly check-ins:', error);
-            }
+    socket.on('checkOut', () => {
+        // Check if the user is checked in and has a valid socket ID
+        if (checkedInUsers.has(socket.id)) {
+            checkedInUsers.delete(socket.id);
+            totalCheckIns--;
+            io.emit('updateCount', totalCheckIns);
         }
-
-        logDailyCheckIns();
-        logHourlyCheckIns();
-    } catch (error) {
-        console.error('Error in socket connection:', error);
-    }
+    });
 });
 
-function getCurrentDate() {
-    const now = new Date();
-    const currentDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    return currentDate;
-}
-
-function getCurrentHour() {
-    return new Date().getHours();
-}
-
+// Haversine formula to calculate distance between two points on the Earth's surface
 function getDistance(location1, location2) {
-    const R = 6371;
+    const R = 6371; // Radius of the Earth in kilometers
     const lat1 = location1.latitude;
     const lon1 = location1.longitude;
     const lat2 = location2.latitude;
@@ -246,11 +143,12 @@ function getDistance(location1, location2) {
         Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c;
+    const distance = R * c; // Distance in kilometers
 
-    return distance * 1000;
+    return distance * 1000; // Convert to meters
 }
 
+// Function to convert degrees to radians
 function toRadians(degrees) {
     return degrees * (Math.PI / 180);
 }
