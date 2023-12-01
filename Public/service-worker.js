7fca991f453d43cd43b83f3e26dd57402ec22197
dolphins <dolphins@dolphinss-MacBook-Air.local>
2023-12-01 11:40:33 -0500

    back to before

--- Public/service-worker.js
+++ Public/service-worker.js
@@ -1,38 +1,19 @@
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('your-cache-name').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                // Add other files you want to cache
-            ]);
-        })
+      caches.open('your-cache-name').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          // Add other files you want to cache
+        ]);
+      })
     );
-});
-
-self.addEventListener('fetch', event => {
+  });
+  
+  self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            // If the requested resource is found in the cache, return it
-            if (response) {
-                return response;
-            }
-
-            // If the requested resource is not found in the cache, fetch it from the network
-            return fetch(event.request).then(networkResponse => {
-                // Check if the network request was successful
-                if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
-                    return networkResponse;
-                }
-
-                // If the network request was successful, add the resource to the cache
-                const clonedResponse = networkResponse.clone();
-                caches.open('your-cache-name').then(cache => {
-                    cache.put(event.request, clonedResponse);
-                });
-
-                return networkResponse;
-            });
-        })
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
     );
-});
+  });
