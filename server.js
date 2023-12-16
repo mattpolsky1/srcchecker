1aa0099b99dd55b35133ccb3e156983cbbaf20d5
dolphins <dolphins@dolphinss-MacBook-Air.local>
2023-12-16 15:54:59 -0500

    attempt

--- server.js
+++ server.js
@@ -75,7 +75,27 @@ io.on('connection', async (socket) => {
         if (checkedInUsers.has(socket.id)) {
             socket.emit('alreadyCheckedIn');
         }
-
+        setInterval(() => {
+            const currentTime = Date.now();
+            const checkInThreshold = 30 * 1000; // 30 seconds
+        
+            checkedInUsers.forEach((checkInTime, socketId) => {
+                const timeSinceCheckIn = currentTime - checkInTime;
+        
+                if (timeSinceCheckIn > checkInThreshold) {
+                    // Auto-checkout the user
+                    checkedInUsers.delete(socketId);
+                    totalCheckIns--;
+                    io.emit('updateCount', totalCheckIns);
+                    io.to(socketId).emit('checkedOutAutomatically');
+        
+                    // Remove or update relevant cookies for the checked-out user
+                    Cookies.remove('checkInStatus');
+        
+                    console.log(`User with socket ID ${socketId} checked out automatically.`);
+                }
+            });
+        }, 1000);
         socket.on('checkIn', async (userLocation) => {
             try {
                 updatePeopleCount();
