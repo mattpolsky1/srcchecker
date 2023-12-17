702eb7b652e9c2067b46952cea5af5b53c12a63d
dolphins <dolphins@dolphinss-MacBook-Air.local>
2023-12-16 20:26:59 -0500

    think this will work

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const http = require('http');
 const socketIO = require('socket.io');
 const path = require('path');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const { v4: uuidv4 } = require('uuid'); 
 
 const uri = "mongodb+srv://mattpolsky:Manning01!@cluster0.ev0u1hj.mongodb.net/CampusHoops?retryWrites=true&w=majority";
 
@@ -39,9 +38,7 @@ const publicPath = path.join(__dirname, 'Public');
 setInterval(() => {
     checkForAutoCheckOut();
 }, 1000);
-function generateSessionId() {
-    return uuidv4();
-}
+
 function checkForAutoCheckOut() {
     const currentTime = Date.now();
 
@@ -98,11 +95,6 @@ function updatePeopleCount() {
 
 io.on('connection', async (socket) => {
     try {
-        const sessionId = generateSessionId();
-
-        // Emit the session ID to the connected client
-        socket.emit('initSession', sessionId);
-
         socket.emit('initCount', totalCheckIns);
 
         if (checkedInUsers.has(socket.id)) {
