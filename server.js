6774af441c48ad56550d5ced2a783ba2169ac8c4
dolphins <dolphins@dolphinss-air.wireless-1x.unc.edu>
2024-01-25 10:06:41 -0500

    35

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-let totalCheckIns = 30;
+let totalCheckIns = 35;
 let lastCheckInTime = 0;
 
 const checkedInUsers = new Map();
