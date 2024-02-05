4f36b8085968b8d9657173917c7eeb2f1b3eada3
dolphins <dolphins@dolphinss-MacBook-Air.local>
2024-02-05 18:06:44 -0500

    online

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-let totalCheckIns = 35;
+let totalCheckIns = 31;
 let lastCheckInTime = 0;
 
 const checkedInUsers = new Map();
