a083b9379769885be9f5fcc237aba5a5e3712ee5
dolphins <dolphins@dolphinss-MacBook-Air.local>
2024-02-20 20:16:42 -0500

    14

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 
 let isAutoCheckoutInProgress = false;
-let totalCheckIns = 1;
+let totalCheckIns = 14;
 let lastCheckInTime = 0;
 
 const checkedInUsers = new Map();
